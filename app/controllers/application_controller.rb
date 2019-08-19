488d3117ef75902b6ca9be906e80889f42ecef3e
anjelicakay <aktowner@gmail.com>
2019-08-18 21:04:05 -0500

    Add devise views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,28 @@
 class ApplicationController < ActionController::Base
-  # protect_from_forgery with: :null_session
+
+  protect_from_forgery
+
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  after_action :verify_authorized, unless: :devise_controller?
+
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password])
+  end
+
+  def after_sign_out_path_for(resource)
+    new_user_registration_path
+  end
+
+  private
+
+  def user_not_authorized
+    flash[:alert] = "You are not authorized to perform this action."
+    redirect_to(fallback_location: root_path)
+  end
+    
 end
